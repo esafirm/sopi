69ef5e8a38d9b87e56037969210cae6f98a5ad15
Esa Firman <esafirm21@gmail.com>
2018-05-27 22:53:09 +0700

    First working code

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const request = require('request')
+
+const url = process.argv[2]
+
+if(!url){
+    console.log('Please input the url first ❌')
+    return 
+}
+
+const lastDotPos = url.lastIndexOf('.')
+const itemId = url.substring(lastDotPos + 1)
+
+const sub = url.substring(0, lastDotPos)
+const shopId = sub.substring(sub.lastIndexOf('.') + 1)
+
+const completeUrl = `https://shopee.co.id/api/v2/item/get\?itemid\=${itemId}\&shopid\=${shopId}`
+
+console.log('complete: ', completeUrl)
+
+request.get(completeUrl, (_, res, body) => {
+    console.log('Request complete ✅')
+    const parsed = JSON.parse(body)
+    const images = parsed.item.images
+
+    console.log('images', images)
+
+    const fs = require('fs')
+    images.forEach(image => {
+        var file = fs.createWriteStream(`${__dirname}/${image}.jpg`);
+        request.get(`https://cf.shopee.co.id/file/${image}`).pipe(file)
+    });
+
+    console.log('Completed ✅')
+})
