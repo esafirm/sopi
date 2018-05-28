8131584a247aed687a14dc030f39a0987c04a09e
Esa Firman <esafirm21@gmail.com>
2018-05-28 19:46:36 +0700

    Add output path and numbering option

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 #!/usr/bin/env node
 
-const request = require('request')
+const program = require('commander');
 
-const url = process.argv[2]
+program
+    .usage('<url> <options>')
+    .version('1.0.0')
+    .option('-o, --output <path>', 'set the output directory')
+    .option('-nn, --no-numbering', 'disable auto numbering')
+    .parse(process.argv)
 
-if(!url){
+const url = program.args[0]
+
+function showErrorMessage() {
     console.log('Please input the url first ❌')
-    return 
+    console.log('\n')
+    console.log(program.outputHelp())
+    process.exit(0)
 }
+
+if (!url) {
+    showErrorMessage();
+    return
+}
+
+const urlParser = require('url')
+const parsedUrl = urlParser.parse(url)
+
+if (!parsedUrl.hostname) {
+    showErrorMessage();
+    return
+}
+
+const request = require('request')
 
 const lastDotPos = url.lastIndexOf('.')
 const itemId = url.substring(lastDotPos + 1)
@@ -24,13 +48,16 @@ request.get(completeUrl, (_, res, body) => {
     const parsed = JSON.parse(body)
     const images = parsed.item.images
 
-    console.log('images', images)
-
+    const disableAutoNumbering = program.numbering
+    const outputDirectory = program.output ? program.output : process.cwd()
     const fs = require('fs')
-    images.forEach(image => {
-        var file = fs.createWriteStream(`${process.cwd()}/${image}.jpg`);
+
+    images.forEach((image, index) => {
+        let fileName = `${outputDirectory}/${disableAutoNumbering ? index : image}.jpg`
+        let file = fs.createWriteStream(fileName)
         request.get(`https://cf.shopee.co.id/file/${image}`).pipe(file)
     });
 
+    console.log(`Downloaded ${images.length} iamge(s)`)
     console.log('Completed ✅')
 })
