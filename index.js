ae4ef9f373af26f6007b035755bdf482583b8ddb
Esa Firman <esafirm21@gmail.com>
2018-10-23 11:00:28 +0700

    Remove node modules

--- index.js
+++ index.js
@@ -3,61 +3,83 @@
 const program = require('commander');
 
 program
-    .usage('<url> <options>')
-    .version('1.0.0')
-    .option('-o, --output <path>', 'set the output directory')
-    .option('-nn, --no-numbering', 'disable auto numbering')
-    .parse(process.argv)
+  .usage('<url> <options>')
+  .version('1.0.0')
+  .option('-c', '--current', 'use current directory')
+  .option('-o, --output <path>', 'set the output directory')
+  .option('-nn, --no-numbering', 'disable auto numbering')
+  .parse(process.argv);
 
-const url = program.args[0]
+const url = program.args[0];
 
 function showErrorMessage() {
-    console.log('Please input the url first ❌')
-    console.log('\n')
-    console.log(program.outputHelp())
-    process.exit(0)
+  console.log('Please input the url first ❌');
+  console.log('\n');
+  console.log(program.outputHelp());
+  process.exit(0);
 }
 
 if (!url) {
-    showErrorMessage();
-    return
+  showErrorMessage();
+  return;
 }
 
-const urlParser = require('url')
-const parsedUrl = urlParser.parse(url)
+const urlParser = require('url');
+const parsedUrl = urlParser.parse(url);
 
 if (!parsedUrl.hostname) {
-    showErrorMessage();
-    return
+  showErrorMessage();
+  return;
 }
 
-const request = require('request')
+if (parsedUrl.hostname.toLowerCase().includes('tokopedia')) {
+  const topedProcessor = require('./dist/TopedProcessor');
+  topedProcessor.process(url);
+  return;
+}
 
-const lastDotPos = url.lastIndexOf('.')
-const itemId = url.substring(lastDotPos + 1)
+const request = require('request');
 
-const sub = url.substring(0, lastDotPos)
-const shopId = sub.substring(sub.lastIndexOf('.') + 1)
+const lastDotPos = url.lastIndexOf('.');
+const itemId = url.substring(lastDotPos + 1);
 
-const completeUrl = `https://shopee.co.id/api/v2/item/get\?itemid\=${itemId}\&shopid\=${shopId}`
+const sub = url.substring(0, lastDotPos);
+const shopId = sub.substring(sub.lastIndexOf('.') + 1);
 
-console.log('complete: ', completeUrl)
+const itemName = url
+  .substring(url.lastIndexOf('/') + 1)
+  .replace(/\./g, '_')
+  .replace(/\-/g, '_');
+
+const completeUrl = `https://shopee.co.id/api/v2/item/get\?itemid\=${itemId}\&shopid\=${shopId}`;
+
+console.log('complete: ', completeUrl);
 
 request.get(completeUrl, (_, res, body) => {
-    console.log('Request complete ✅')
-    const parsed = JSON.parse(body)
-    const images = parsed.item.images
+  console.log('Request complete ✅');
+  const parsed = JSON.parse(body);
+  const images = parsed.item.images;
 
-    const disableAutoNumbering = program.numbering
-    const outputDirectory = program.output ? program.output : process.cwd()
-    const fs = require('fs')
+  const disableAutoNumbering = program.numbering;
+  const defaultDir = program.current ? process.cwd() : itemName;
+  const outputDirectory = program.output ? program.output : defaultDir;
+  const fs = require('fs');
 
-    images.forEach((image, index) => {
-        let fileName = `${outputDirectory}/${disableAutoNumbering ? index : image}.jpg`
-        let file = fs.createWriteStream(fileName)
-        request.get(`https://cf.shopee.co.id/file/${image}`).pipe(file)
-    });
+  // If directory doesn't exist, create
+  if (outputDirectory !== process.cwd()) {
+    if (!fs.existsSync(outputDirectory)) {
+      fs.mkdirSync(outputDirectory);
+    }
+  }
 
-    console.log(`Downloaded ${images.length} iamge(s)`)
-    console.log('Completed ✅')
-})
+  images.forEach((image, index) => {
+    let fileName = `${outputDirectory}/${
+      disableAutoNumbering ? index : image
+    }.jpg`;
+    let file = fs.createWriteStream(fileName);
+    request.get(`https://cf.shopee.co.id/file/${image}`).pipe(file);
+  });
+
+  console.log(`Downloaded ${images.length} iamge(s)`);
+  console.log('Completed ✅');
+});
