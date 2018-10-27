58a6d845c46481b1f96588d9dea118ed0acdd5f3
Esa Firman <esafirm21@gmail.com>
2018-10-27 08:25:41 +0700

    Add description prefix and suffix. Configurable

--- src/MetaGenerator.js
+++ src/MetaGenerator.js
@@ -2,12 +2,14 @@
 
 const fs = require('fs');
 const idGenerator = require('./IdGenerator');
+const config = require('../config');
 
 // @param body response body of sophee item
 module.exports.write = function(url, parsedBody, outputDir) {
-  const desc = `${parsedBody.item.description}\n\nID: ${idGenerator.generate(
-    url
-  )}`;
+  const id = '\n\nID: ${idGenerator.generate(url)';
+  const desc =
+    config.descPrefix + parsedBody.item.description + id + config.descSuffix;
+
   createFile(`${outputDir}/desc`, desc);
 };
 
